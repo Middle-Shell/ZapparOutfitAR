69841fa8e3867367cb7fa76d7e44e6c4fc376a57
Middle Shell <denob311302@gmail.com>
2023-08-12 15:22:34 +0300

    Draw-avl

--- Assets/_Project/Scripts/RayMove.cs
+++ Assets/_Project/Scripts/RayMove.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class RayPaint : MonoBehaviour
 {
-    public Texture2D brushTexture; // Текстура кисти
-    public float brushSize = 0.1f; // Размер кисти
-    public LayerMask paintableLayer; // Слой, на который можно наносить краску
+    public float brushSize = 0.1f;
+    public LayerMask paintableLayer;
 
     private MaterialPropertyBlock propBlock;
+    private Texture2D currentTexture;
+    private Texture2D tempTexture;
 
     private void Start()
     {
         propBlock = new MaterialPropertyBlock();
+        Renderer renderer = GetComponent<Renderer>();
+        currentTexture = renderer.material.mainTexture as Texture2D;
+        tempTexture = new Texture2D(currentTexture.width, currentTexture.height);
+        PaintOnTexture(tempTexture, Vector2.one, 1, new Color(0,0,0,0));
     }
 
     private void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            Vector2 centerScreenPoint = new Vector2(Screen.width / 2, Screen.height / 2); // Центр экрана
-
-            Ray ray = Camera.main.ScreenPointToRay(centerScreenPoint);
+            Vector2 centerScreenPoint = new Vector2(Screen.width / 2, Screen.height / 2);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, paintableLayer))
@@ -34,39 +38,41 @@ public class RayPaint : MonoBehaviour
                     Vector2 pixelUV = hit.textureCoord;
                     print(pixelUV);
 
-                    Texture2D currentTexture = hitRenderer.material.mainTexture as Texture2D;
-                    Color[] textureData = currentTexture.GetPixels();
+                    // Применяем рисование на временную текстуру
+                    PaintOnTexture(tempTexture, pixelUV, brushSize, Color.blue);
 
-                    int textureWidth = currentTexture.width;
-                    int textureHeight = currentTexture.height;
-                    print(textureHeight);
+                    // Применяем изменения из временной текстуры на текущую
+                    currentTexture.SetPixels(tempTexture.GetPixels());
+                    currentTexture.Apply();
 
-                    int centerX = Mathf.FloorToInt(pixelUV.x * textureWidth);
-                    int centerY = Mathf.FloorToInt(pixelUV.y * textureHeight);
-                    print(centerX);
-
-                    int radius = Mathf.RoundToInt(brushSize * Mathf.Max(textureWidth, textureHeight));
-
-                    for (int y = centerY - radius; y <= centerY + radius; y++)
-                    {
-                        for (int x = centerX - radius; x <= centerX + radius; x++)
-                        {
-                            if (x >= 0 && x < textureWidth && y >= 0 && y < textureHeight)
-                            {
-                                int index = y * textureWidth + x;
-                                textureData[index] = Color.black; // Измените на нужный цвет
-                            }
-                        }
-                    }
-
-                    Texture2D newTexture = new Texture2D(textureWidth, textureHeight);
-                    newTexture.SetPixels(textureData);
-                    newTexture.Apply();
-
-                    propBlock.SetTexture("_MainTex", newTexture);
+                    propBlock.SetTexture("_MainTex", currentTexture);
                     hitRenderer.SetPropertyBlock(propBlock);
                 }
             }
         }
+    }
+
+    // Рисование на временной текстуре
+    private void PaintOnTexture(Texture2D texture, Vector2 center, float radius, Color color)
+    {
+        int centerX = Mathf.FloorToInt(center.x * texture.width);
+        int centerY = Mathf.FloorToInt(center.y * texture.height);
+        int brushRadius = Mathf.RoundToInt(radius * Mathf.Max(texture.width, texture.height));
+
+        Color[] textureData = texture.GetPixels();
+        for (int y = centerY - brushRadius; y <= centerY + brushRadius; y++)
+        {
+            for (int x = centerX - brushRadius; x <= centerX + brushRadius; x++)
+            {
+                if (x >= 0 && x < texture.width && y >= 0 && y < texture.height)
+                {
+                    int index = y * texture.width + x;
+                    textureData[index] = color;
+                }
+            }
+        }
+
+        texture.SetPixels(textureData);
+        texture.Apply();
     }
 }
